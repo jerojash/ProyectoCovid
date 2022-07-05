959afcc97e1b2007b22cd115d2222e53764038c2
Manuel Aguilar <manu.ale.agui.rivas@gmail.com>
2022-07-05 14:38:17 -0400

    configuración conexión

--- src/proyectocovid/ProyectoCovid.java
+++ src/proyectocovid/ProyectoCovid.java
@@ -4,6 +4,8 @@
  * and open the template in the editor.
  */
 package proyectocovid;
+import SQL.ConexionSQL;
+import java.util.Scanner;
 import javax.swing.JFrame;
 import view.registrarPersonaContagiada;
 import view.registrarVacuna;
@@ -21,23 +23,29 @@ public class ProyectoCovid {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        ConexionSQL conexion = new ConexionSQL();
+        conexion.connected();
+        Scanner entrada = new Scanner(System.in);
+        String entradaS = entrada.nextLine();
+        conexion.disconnect();
+        
         registrarPersonaContagiada ventana = new registrarPersonaContagiada();
-        ventana.setVisible(true);
+        ventana.setVisible(false);
         ventana.setLocationRelativeTo(null);
         registrarVacuna ventana1 = new registrarVacuna();
-        ventana1.setVisible(true);
+        ventana1.setVisible(false);
         ventana1.setLocationRelativeTo(null);
         registrarPersonaVacunada ventana2 = new registrarPersonaVacunada();
-        ventana2.setVisible(true);
+        ventana2.setVisible(false);
         ventana2.setLocationRelativeTo(null);
         registrarPersonalSalud ventana3 = new registrarPersonalSalud();
-        ventana3.setVisible(true);
+        ventana3.setVisible(false);
         ventana3.setLocationRelativeTo(null);
         registrarTratamiento ventana4 = new registrarTratamiento();
-        ventana4.setVisible(true);
+        ventana4.setVisible(false);
         ventana4.setLocationRelativeTo(null);
         registrarTratamientoPaciente ventana5 = new registrarTratamientoPaciente();
-        ventana5.setVisible(true);
+        ventana5.setVisible(false);
         ventana5.setLocationRelativeTo(null);
     }
     
